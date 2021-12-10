36262c69749d3821c2064b6ba71ac0236029dafc
G-Chomphuthip <95874610+G-Chomphuthip@users.noreply.github.com>
2021-12-09 21:54:30 -0800

    Update ubuntuscript.sh

--- ubuntuscript.sh
+++ ubuntuscript.sh
@@ -7,6 +7,38 @@ warningdays=7
 echo "Setting maximum password age to $maxpwage, minimum password age to $minpwage, and the warning days to $warningdays"
 chage -M $maxpwage -m $minpwage -W $warningdays
 
+echo "checking for rsh-server..."
+if [ -z $( dpkg -l | grep rsh-server ) ]
+then
+  echo "rsh-server is not installed"
+else
+  echo "rsh-server is installed. uninstalling..."
+  sudo apt-get remove rsh-server
+fi
+
+echo "checking for nis..."
+if [ -z $( dpkg -l | grep nis ) ]
+then
+  echo "nis is not installed"
+else
+  echo "nis is installed. uninstalling..."
+  sudo apt-get remove nis
+fi
+
+echo "checking for libpam-pkcs11..."
+if [ -z $( dpkg -l | grep libpam-pkcs11 ) ]
+then
+  echo "libpam-pkcs11 is not installed. installing..."
+  sudo apt install libpam-pkcs11 -y
+  if [ -f /etc/etc/pam_pkcs11/pam_pkcs11.conf ]
+  then
+    echo use_mappers=pwent >> /etc/etc/pam_pkcs11/pam_pkcs11.conf
+  else
+    echo 'YOURE GOING TO NEED TO DO SOME MORE CONFIGURATION OF PKCS11'
+else
+  echo "libpam-pkcs11 is installed"
+fi
+
 echo "Finding all accounts with no password (ADD A PASSWORD) and logging to 'pwlessaccs'"
 awk -F: '($2 == "") {print}' /etc/shadow | tee -a "pwlessaccs"
 
